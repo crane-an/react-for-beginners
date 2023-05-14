426cb7de6f4490e747bcea136ce5db0be2610cc0
crane-an <changhak1026@gmail.com>
2023-05-14 23:01:35 +0900

    modified Commit

--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -9,26 +10,31 @@ function Home() {
       await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
       ).json();
     setMovies(json.data.movies);
-    setLoading(false);  
+    setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-  
+
   return (
-    
-    <div>
-      {loading ? <h1>Loading...</h1> : 
-        <div>{movies.map(movie => 
-          <Movie 
-            key={movie.id} 
-            id={movie.id}
-            coverImg={movie.medium_cover_image} 
-            title={movie.title} 
-            summary={movie.summary} 
-            genres={movie.genres}/>
-        )}</div>}
-  
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div> ) : ( 
+        <div className={styles.movies}>
+          {movies.map(movie => 
+            <Movie 
+              key={movie.id} 
+              id={movie.id}
+              coverImg={movie.medium_cover_image} 
+              title={movie.title} 
+              year={movie.year}
+              summary={movie.summary} 
+              genres={movie.genres}/>
+          )}
+        </div>
+        )}
     </div>
   );
 }
